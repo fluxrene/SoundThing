f09a4e67f3bb239285e8979988f24ffebad1bd47
fluxrene <rene@fluxlabs.io>
2017-05-14 11:42:21 +0800

    first stuff

--- app/src/main/java/me/soundthing/soundthing/MainActivity.java
+++ app/src/main/java/me/soundthing/soundthing/MainActivity.java
@@ -1,5 +1,8 @@
 package me.soundthing.soundthing;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +13,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+  Thread t;
+  int sr = 44100;
+  boolean isRunning = true;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +30,10 @@ public class MainActivity extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+        // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //        .setAction("Action", null).show();
+        isRunning = !isRunning;
+        makeSomeSound();
       }
     });
   }
@@ -48,5 +58,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  private void makeSomeSound() {
+    // start a new thread to synthesise audio
+    t = new Thread() {
+      public void run() {
+        // set process priority
+        setPriority(Thread.MAX_PRIORITY);
+        // set the buffer size
+        int buffsize = AudioTrack.getMinBufferSize(sr,
+                AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        // create an audiotrack object
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sr, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, buffsize,
+                AudioTrack.MODE_STREAM);
+
+        short samples[] = new short[buffsize];
+        int amp = 10000;
+        double twopi = 8. * Math.atan(1.);
+        double fr = 440.f;
+        double ph = 0.0;
+
+        // start audio
+        audioTrack.play();
+
+        // synthesis loop
+        while (isRunning) {
+          fr = 2040;
+          for (int i = 0; i < buffsize; i++) {
+            samples[i] = (short) (amp * Math.sin(ph));
+            ph += twopi * fr / sr;
+          }
+          audioTrack.write(samples, 0, buffsize);
+        }
+        audioTrack.stop();
+        audioTrack.release();
+      }
+    };
+    t.start();
   }
 }
